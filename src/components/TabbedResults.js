444c7252b94214e194e47d0b0d3352ac0198aa6e
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2017-07-13 18:44:04 +0200

    - tabs component correctly receiving the needed props

--- src/components/TabbedResults.js
+++ src/components/TabbedResults.js
@@ -6,7 +6,7 @@ class TabbedResults extends Component {
     constructor(props){
         super(props);
         this.state = {
-            selectedBoxes: props.selectedBoxes,
+            selectedBoxes: [],
             fromDate: null,
             fromHours: null,
             toDate: null,
@@ -14,10 +14,22 @@ class TabbedResults extends Component {
         };
     }
 
-    componentDidUpdate(prevProps, prevState){
-        console.log("tabs props: ",this.props);
-        if (this.state.selectedBoxes !== this.props.selectedBoxes) {
+    componentDidUpdate(){
+        if (
+            this.state.selectedBoxes !== this.props.selectedBoxes ||
+            this.state.fromDate !== this.props.fromDate ||
+            this.state.fromHours !== this.props.fromHours ||
+            this.state.toDate !== this.props.toDate ||
+            this.state.toHours !== this.props.toHours
+        ) {
             this.setState({selectedBoxes: this.props.selectedBoxes});
+            this.setState({fromDate: this.props.fromDate});
+            this.setState({toDate: this.props.toDate});
+            this.setState({fromHours: this.props.fromHours});
+            this.setState({toHours: this.props.toHours});
+            const hostname = window.location.href.includes('scll') || window.location.href.includes('10.105') ? 'urbanclimate-rest.app.scll' : '62.86.126.18:8080';
+            console.log(this.state);
+            //const URL = `http://${hostname}/get_all_obs_from_date_to_date_for_obs_sys?obsys=${boxNameSearched.toUpperCase()}&datafrom=${dateFrom}&h_from=${hFrom}&datato=${dateTo}&h_to=${hTo}`;
         }
     }
 
--- src/containers/TabbedResultsContainer.js
+++ src/containers/TabbedResultsContainer.js
@@ -4,7 +4,10 @@ import TabbedResults from '../components/TabbedResults';
 const mapStateToProps = (state) => {
     return {
         selectedBoxes: state.selectedBoxes,
-        fromDate: state.form.MaterialUiForm.values !== undefined ? state.form.MaterialUiForm.values.fromDate : null
+        fromDate: state.form.MaterialUiForm.values !== undefined ? state.form.MaterialUiForm.values.fromDate : null,
+        toDate: state.form.MaterialUiForm.values !== undefined ? state.form.MaterialUiForm.values.toDate : null,
+        fromHours: state.form.MaterialUiForm.values !== undefined ? state.form.MaterialUiForm.values.fromHours : null,
+        toHours: state.form.MaterialUiForm.values !== undefined ? state.form.MaterialUiForm.values.toHours : null
     }
 };
 
