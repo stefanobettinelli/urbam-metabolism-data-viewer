f983729588fca0cad5e5b8bcf06ae31b6f49cf2c
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2017-07-19 11:36:51 +0200

    - changed redux state from fetchFilteredObservation to fetchedFilteredObservation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,13 @@
 import {combineReducers} from 'redux';
 import selectedBoxes from './selectedBoxes';
 import fetchedBoxList from './fetchedBoxList';
-import fetchFilteredObservation from './fetchedFilteredObservation';
+import fetchedFilteredObservation from './fetchedFilteredObservation';
 import {reducer as formReducer} from 'redux-form';
 
 const rootReducer = combineReducers({
     selectedBoxes,
     fetchedBoxList,
-    fetchFilteredObservation,
+    fetchedFilteredObservation,
     form: formReducer
 });
 
