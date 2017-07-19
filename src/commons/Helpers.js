28f04b8812d4bc149b3ee19ac99b188030735d22
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2017-07-19 11:40:57 +0200

    - added cest and utc timestamps as columns

--- src/commons/Helpers.js
+++ src/commons/Helpers.js
@@ -1,5 +1,5 @@
 const convertArrayOfObjectsToCSV = (args) => {
-    let csvStringResult = "box_name; timestamp; co; co2; no2; o3; pm2.5; pm10; voc; air_t_ds; air_t_am; rad_am\n";
+    let csvStringResult = "box_name; timestamp_cest; timestamp_utc; co; co2; no2; o3; pm2.5; pm10; voc; air_t_ds; air_t_am; rad_am\n";
 
     args.forEach(
         (element) => {
