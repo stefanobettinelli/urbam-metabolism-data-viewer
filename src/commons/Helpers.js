851f2bc23832954c973c92f76b7f3c135b59fc51
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2017-07-20 18:47:39 +0200

    fixed also the exported data in the csv file - was using mounth instead of minutes

--- src/commons/Helpers.js
+++ src/commons/Helpers.js
@@ -1,8 +1,15 @@
+import moment from 'moment';
+
 const convertArrayOfObjectsToCSV = (args) => {
     let csvStringResult = "box_name; timestamp_cest; timestamp_utc; co; co2; no2; o3; pm2.5; pm10; voc; air_t_ds; air_t_am; rad_am\n";
 
     args.forEach(
         (element) => {
+            let colValues = element.split(';');
+            colValues.forEach(item => item.trim());
+            colValues[1] = moment(colValues[1]).format('YYYY-MM-DD HH:mm:ss');
+            colValues[2] = moment(colValues[2]).format('YYYY-MM-DD HH:mm:ss');
+            element = colValues.join(';');
             csvStringResult += element + "\n";
         }
     );
