0282127a2b57a5f212f841494fb78bc63e6c9d86
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2017-07-21 18:23:26 +0200

    - mouting react tables on the tabs

--- src/commons/Helpers.js
+++ src/commons/Helpers.js
@@ -1,4 +1,4 @@
-const convertArrayOfObjectsToCSV = (args) => {
+export const convertArrayOfObjectsToCSV = (args) => {
     let csvStringResult = "box_name, timestamp, co, co2, no2, o3, pm2.5, pm10\n";
 
     args.forEach(
@@ -10,4 +10,12 @@ const convertArrayOfObjectsToCSV = (args) => {
     return csvStringResult;
 };
 
-export {convertArrayOfObjectsToCSV};
+export const convertCSVtoJSONArray = (csvText) => {
+    if (!csvText) return;
+    const records = csvText.split("<br/>");
+    const header = records[0].split(";").map(
+        item => ( {Header: item, accessor: item.toLowerCase()} )
+    );
+
+    return header;
+};
--- src/components/TabResult.js
+++ src/components/TabResult.js
@@ -1,4 +1,8 @@
 import React, {Component} from "react";
+import ReactTable from 'react-table';
+import {convertCSVtoJSONArray} from '../commons/Helpers';
+
+import 'react-table/react-table.css';
 
 class TabResult extends Component {
 
@@ -30,11 +34,50 @@ class TabResult extends Component {
 
     render() {
         const {boxName, csv} = this.props;
+
+        const header = convertCSVtoJSONArray(csv);
+        console.log("CSV header ", header);
+
+        const data = [
+            {
+                name: 'Tanner Linsley',
+                age: 26,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                }
+            },
+            {
+                name: 'Tanner Linsley',
+                age: 26,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                }
+            }
+        ];
+
+        const columns = [{
+            Header: 'Name',
+            accessor: 'name' // String-based value accessors!
+        }, {
+            Header: 'Age',
+            accessor: 'age',
+            Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+        }, {
+            id: 'friendName', // Required because our accessor is not a string
+            Header: 'Friend Name',
+            accessor: d => d.friend.name // Custom value accessors!
+        }, {
+            Header: props => <span>Friend Age</span>, // Custom header components!
+            accessor: 'friend.age'
+        }]
+
         return (
-            <div key={String(boxName)} label={boxName}>
-                <h1> {boxName} </h1> <br/>
-                {/*{csv}*/}
-            </div>
+            <ReactTable
+                data={data}
+                columns={columns}
+            />
         );
     }
 
