a4727740eda89911e604c7c3814d1f14205f6b81
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2017-07-28 15:38:41 +0200

    index.js added dev. and prod. env setup

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,8 @@ import thunkMiddleware from 'redux-thunk';
 import {createLogger} from 'redux-logger';
 import rootReducer from '../reducers';
 
+
+//dev. env setup
 const loggerMiddleware = createLogger();
 const store = createStore(
     rootReducer,
@@ -11,5 +13,13 @@ const store = createStore(
         loggerMiddleware // neat middleware that logs actions
     )
 );
+
+//prod. env setup
+// const store = createStore(
+//     rootReducer,
+//     applyMiddleware(
+//         thunkMiddleware, // lets us dispatch() functions
+//     )
+// );
 
 export default store;
