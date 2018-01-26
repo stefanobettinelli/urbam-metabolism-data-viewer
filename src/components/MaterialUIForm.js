c3037f5c7326db2ade38cdde6b20d63a86d936cb
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2018-01-26 12:20:17 +0100

    Feat: Time Zone selector
    added Time Zone selector, the selected date and time will be relative to the selected TZ

--- src/components/MaterialUIForm.js
+++ src/components/MaterialUIForm.js
@@ -1,76 +1,103 @@
-import React from "react";
-import {Field, reduxForm} from "redux-form";
-import {DatePicker, TimePicker} from "redux-form-material-ui";
-import RaisedButton from "material-ui/RaisedButton";
-import MultipleSelectContainer from "../containers/MultipleSelectContainer";
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { DatePicker, TimePicker } from 'redux-form-material-ui';
+import RaisedButton from 'material-ui/RaisedButton';
+import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
+import MultipleSelectContainer from '../containers/MultipleSelectContainer';
 import store from '../store';
-import {clearSelectedBoxList} from "../actions";
+import { clearSelectedBoxList } from '../actions';
 
-const renderBoxList = (props) => {
-    return (
-        <MultipleSelectContainer/>
-    );
+const renderBoxList = props => {
+  return <MultipleSelectContainer />;
+};
+
+const styles = {
+  radioButton: {
+    marginBottom: 16
+  }
 };
 
 const MaterialUiForm = props => {
-    const {handleSubmit, pristine, reset, submitting} = props;
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <Field name="boxList" component={renderBoxList}/>
-            </div>
-            <div>
-                <Field
-                    name="fromDate"
-                    component={DatePicker}
-                    format={null}
-                    hintText="From Date?"
-                    // onChange={() => store.dispatch()}
-                />
-            </div>
-            <div>
-                <Field
-                    name="fromHours"
-                    autoOk={true}
-                    component={TimePicker}
-                    format={null}
-                    props={{format: "24hr"}}
-                    hintText="From Hours?"
-                />
-            </div>
-            <div>
-                <Field
-                    name="toDate"
-                    component={DatePicker}
-                    format={null}
-                    hintText="To Date?"
-                />
-            </div>
-            <div>
-                <Field
-                    name="toHours"
-                    autoOk={true}
-                    component={TimePicker}
-                    format={null}
-                    props={{format: "24hr"}}
-                    hintText="To Hours?"
-                />
-            </div>
-            <div>
-                <RaisedButton
-                    onTouchTap={
-                        () => {
-                            reset();
-                            store.dispatch(clearSelectedBoxList())
-                        }
-                    }
-                    label="Clear values" type="button" style={{margin: 12}}
-                />
-            </div>
-        </form>
-    )
+  const { handleSubmit, pristine, reset, submitting } = props;
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <Field name="boxList" component={renderBoxList} />
+      </div>
+      <div>
+        <Field
+          name="fromDate"
+          component={DatePicker}
+          format={null}
+          hintText="From Date?"
+          // onChange={() => store.dispatch()}
+        />
+      </div>
+      <div>
+        <Field
+          name="fromHours"
+          autoOk={true}
+          component={TimePicker}
+          format={null}
+          props={{ format: '24hr' }}
+          hintText="From Hours?"
+        />
+      </div>
+      <div>
+        <Field
+          name="toDate"
+          component={DatePicker}
+          format={null}
+          hintText="To Date?"
+        />
+      </div>
+      <div>
+        <Field
+          name="toHours"
+          autoOk={true}
+          component={TimePicker}
+          format={null}
+          props={{ format: '24hr' }}
+          hintText="To Hours?"
+        />
+      </div>
+      <div>
+        <RadioButtonGroup name="shipSpeed" defaultSelected="utc">          
+          <RadioButton
+            value="utc"
+            label="UTC"
+            style={styles.radioButton}
+          />
+          <RadioButton
+            value="cet"
+            label="CET"
+            style={styles.radioButton}
+          />
+          <RadioButton
+            value="cest"
+            label="CEST"
+            style={styles.radioButton}
+          />
+          <RadioButton
+            value="eu-rome"
+            label="EU-ROME"
+            style={styles.radioButton}
+          />
+        </RadioButtonGroup>
+        <RaisedButton
+          onTouchTap={() => {
+            reset();
+            store.dispatch(clearSelectedBoxList());
+          }}
+          label="Clear values"
+          type="button"
+          style={{ margin: 12 }}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default reduxForm({
-    form: 'MaterialUiForm',  // a unique identifier for this form
-})(MaterialUiForm)
+  form: 'MaterialUiForm' // a unique identifier for this form
+})(MaterialUiForm);
