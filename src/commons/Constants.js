f81c45df71d319b5f7dbbaedca55e0fb4d50f78a
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2018-01-26 13:16:12 +0100

    Update: Constants
    now Constants include local evn REST api URI + on production build URI

--- src/commons/Constants.js
+++ src/commons/Constants.js
@@ -1,3 +1,4 @@
+//export const REST_API_HOSTNAME = window.location.href.includes('scll') || window.location.href.includes('10.105') ? 'urbanclimate-rest.app.scll' : '62.86.126.18:8080';
 export const REST_API_HOSTNAME = 'localhost:8080';
 export const D_FORMAT = 'YYYY-M-D';
 export const T_FORMAT = 'HH:mm';
